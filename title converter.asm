599bb1ea4e0fd16997048ca84736596aeebc3ce2
GABRIEL SENATORE COSTA <gabrielsenatore28@gmail.com>
2025-08-18 22:11:01 -0300

    Update title converter.asm
    uncommented versions

--- title converter.asm
+++ title converter.asm
@@ -1,45 +1,36 @@
-title converter                   ; Título do fonte (informativo para o assembler)
-.model small                      ; Modelo de memória: 1 segmento de código + 1 de dados
-.stack 100h                       ; Reserva 256 bytes para a pilha (stack)
+title converter
+.model small
+.stack 100h
 
 .data
-    mensagem1 db 'Digite uma letra minuscula: $'                ; String exibida pelo serviço 09h (termina em '$')
-    mensagem2 db 13,10, 'Letra maiuscula correspondente: $'     ; 13=CR, 10=LF (pula linha), também termina em '$'
+    mensagem1 db 'Digite uma letra minuscula: $'
+    mensagem2 db 13,10, 'Letra maiuscula correspondente: $'
 
 .code
-main PROC                         ; Início do procedimento principal (ponto de entrada)
+main PROC
+    mov ax, @data
+    mov ds, ax
 
-    mov ax, @data                 ; AX <- endereço do segmento .data
-    mov ds, ax                    ; DS <- AX  (inicializa o segmento de dados)
+    mov ah, 09h
+    lea dx, mensagem1
+    int 21h
 
-    ; -------- imprime a mensagem 1 --------
-    mov ah, 09h                   ; AH=09h -> função do int 21h para imprimir string terminada por '$'
-    lea dx, mensagem1             ; DX <- offset (endereço) de 'mensagem1'
-    int 21h                       ; Chama DOS: imprime a string em DX até o '$'
+    mov ah, 01h
+    int 21h
+    mov bl, al
 
-    ; -------- lê um caractere --------
-    mov ah, 01h                   ; AH=01h -> função do int 21h para ler 1 caractere do teclado (ecoado na tela)
-    int 21h                       ; Chama DOS: AL recebe o código ASCII digitado
-    mov bl, al                    ; BL <- AL (guarda o caractere lido em BL para usar depois)
+    sub bl, 32
 
-    ; -------- converte minúscula -> maiúscula --------
-    ; Em ASCII, 'a'..'z' (97..122) viram 'A'..'Z' (65..90) subtraindo 32
-    sub bl, 32                    ; BL <- BL - 32 (transforma 'a' em 'A', 'b' em 'B', etc.)
+    mov ah, 09h
+    lea dx, mensagem2
+    int 21h
 
-    ; -------- imprime a mensagem 2 --------
-    mov ah, 09h                   ; AH=09h -> imprimir string
-    lea dx, mensagem2             ; DX <- offset de 'mensagem2'
-    int 21h                       ; Chama DOS: imprime a mensagem 2
+    mov dl, bl
+    mov ah, 02h
+    int 21h
 
-    ; -------- imprime o caractere convertido --------
-    mov dl, bl                    ; DL <- BL (o caractere a ser impresso deve ir em DL)
-    mov ah, 02h                   ; AH=02h -> função do int 21h para imprimir 1 caractere em DL
-    int 21h                       ; Chama DOS: imprime o caractere de DL
+    mov ah, 4Ch
+    int 21h
 
-    ; -------- encerra o programa --------
-    mov ah, 4Ch                   ; AH=4Ch -> função do int 21h para terminar o programa
-    int 21h                       ; Retorna ao DOS
-
-main ENDP                         ; Fim do procedimento 'main'
-END main                          ; Diz ao assembler que o ponto de entrada é o rótulo 'main'
-
+main ENDP
+END main
